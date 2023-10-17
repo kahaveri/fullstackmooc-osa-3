209ab5fb804a9628357236d3dcf263e4025f4b24
Katri Haverinen <katri.haverinen@stat.fi>
2023-10-17 16:32:26 +0300

    Tehtävä 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -51,6 +53,17 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const id = Math.floor(Math.random()*5000)
+
+    const person = request.body
+    person.id = id
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
