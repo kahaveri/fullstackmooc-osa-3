224d317a08e1e18b851011e470c065c3cb2c0352
Katri Haverinen <katri.haverinen@stat.fi>
2023-11-09 10:20:02 +0200

    Tehtävä 3.9 (bäkki)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 var morgan = require ('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('data', function(req, res) {return JSON.stringify(req.body)} )
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors())
 
 let persons = [
     {
